fd12addc6e65f52cf959e9d1c7be4a6c29096392
ootr <git@ootr.jp>
2025-01-10 21:28:40 +0900

    ServiceAccountの名前を変更できるように

--- src/app/api/ns/[nsId]/services/accounts/[accountId]/route.ts
+++ src/app/api/ns/[nsId]/services/accounts/[accountId]/route.ts
@@ -11,6 +11,21 @@ export type DeleteExternalServiceAccountResponse =
       error: string;
     };
 
+export type UpdateExternalServiceAccountResponse =
+  | {
+      status: "success";
+      account: {
+        id: string;
+        name: string;
+        service: string;
+        icon?: string;
+      };
+    }
+  | {
+      status: "error";
+      error: string;
+    };
+
 export async function DELETE(
   req: NextRequest,
   { params }: { params: { nsId: string; accountId: string } },
@@ -71,5 +86,86 @@ export async function DELETE(
 
   return NextResponse.json({
     status: "success",
+  });
+}
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { nsId: string; accountId: string } },
+): Promise<NextResponse<UpdateExternalServiceAccountResponse>> {
+  const session = await getServerSession();
+
+  const email = session?.user?.email;
+
+  if (!email) {
+    return NextResponse.json(
+      { status: "error", error: "Not authenticated" },
+      { status: 401 },
+    );
+  }
+
+  const { name } = await req.json();
+
+  if (!name) {
+    return NextResponse.json(
+      { status: "error", error: "Name is required" },
+      { status: 400 },
+    );
+  }
+
+  const namespace = await prisma.namespace.findUnique({
+    where: {
+      id: params.nsId,
+    },
+    include: {
+      owner: true,
+    },
+  });
+
+  if (!namespace) {
+    return NextResponse.json(
+      { status: "error", error: "Namespace not found" },
+      { status: 404 },
+    );
+  }
+
+  if (namespace.owner.email !== email) {
+    return NextResponse.json(
+      { status: "error", error: "Not authorized" },
+      { status: 403 },
+    );
+  }
+
+  const serviceAccount = await prisma.externalServiceAccount.findUnique({
+    where: {
+      id: params.accountId,
+      namespaceId: params.nsId,
+    },
+  });
+
+  if (!serviceAccount) {
+    return NextResponse.json(
+      { status: "error", error: "Service account not found" },
+      { status: 404 },
+    );
+  }
+
+  const updatedAccount = await prisma.externalServiceAccount.update({
+    where: {
+      id: params.accountId,
+    },
+    data: {
+      name,
+    },
+  });
+
+  return NextResponse.json({
+    status: "success",
+    account: {
+      id: updatedAccount.id,
+      name: updatedAccount.name,
+      service: updatedAccount.service,
+      icon: updatedAccount.icon || undefined,
+    },
   });
 }
