c9e66ada347208b3486b52c345eb067a267d1d24
ootr <git@ootr.jp>
2025-09-25 05:25:41 +0900

    テーブルを修正

--- prisma/migrations/20250120190508_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `name` on the `Member` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Member` DROP COLUMN `name`;
